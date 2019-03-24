725311cabc0d4ca59a47df9785c5b37cb896186a
huangsuwei <suweihuang@iyuedian.com>
2019-03-24 09:19:27 +0800

    三个页面基本完成

--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -3,13 +3,54 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    select:[
+      { name: '精选榜', index: 0 },
+      { name: '教官说', index: 1 },
+      { name: '我想问', index: 2 }
+    ],
+    winList:[
+      { 
+        map:'/img/win1.png',
+        bar:"【干货】你应该掌握的溺水急救小常识",
+        time:'2019-3-13',
+        seeNum:3457,
+        collectNum:234
+      },
+      {
+        map: '/img/win2.png',
+        bar: "【干货】你应该掌握的溺水急救小常识",
+        time: '2019-3-13',
+        seeNum: 3457,
+        collectNum: 234
+      },
+      {
+        map: '/img/win3.png',
+        bar: "【干货】你应该掌握的溺水急救小常识",
+        time: '2019-3-13',
+        seeNum: 3457,
+        collectNum: 234
+      },
+      {
+        map: '/img/win4.png',
+        bar: "【干货】你应该掌握的溺水急救小常识",
+        time: '2019-3-13',
+        seeNum: 3457,
+        collectNum: 234
+      },
+      {
+        map: '/img/win5.png',
+        bar: "【干货】你应该掌握的溺水急救小常识",
+        time: '2019-3-13',
+        seeNum: 3457,
+        collectNum: 234
+      },
+    ],
+    atIndex:0
+  },
+  select(e){
+    e.currentTarget.dataset.index == this.data.atIndex ? "" : this.setData({atIndex:e.currentTarget.dataset.index})
   },
   onLoad: function () {
-    // this.setData({
-    //   logs: (wx.getStorageSync('logs') || []).map(log => {
-    //     return util.formatTime(new Date(log))
-    //   })
-    // })
+
   }
 })
